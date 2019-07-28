2bb43346d61c185775510a7f07d617434bfdada0
leejm1 <wnsaud6016@naver.com>
2019-07-28 21:45:54 +0900

    place entitiy

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type Query {\n  sayBye: String!\n  sayHello(name: String!): SayHelloResponse!\n  user: User\n}\n\ntype SayHelloResponse {\n  text: String!\n  error: Boolean!\n}\n\ntype User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  facebookId: String\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhonenNumber: Boolean!\n  profilePhotho: String\n  createdAt: String\n  updatedAt: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  createdAt: String!\n  updateAt: String!\n}\n"];
+export const typeDefs = ["type Query {\n  sayBye: String!\n  sayHello(name: String!): SayHelloResponse!\n  user: User\n}\n\ntype SayHelloResponse {\n  text: String!\n  error: Boolean!\n}\n\ntype Place {\n  id: Int!\n  name: String!\n  lat: Float!\n  lng: Float!\n  address: String!\n  isFav: Boolean!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  facebookId: String\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhonenNumber: Boolean!\n  profilePhotho: String\n  createdAt: String\n  updatedAt: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  createdAt: String!\n  updateAt: String!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -37,6 +37,17 @@ export interface User {
   lastLng: number | null;
   lastLat: number | null;
   lastOrientation: number | null;
+}
+
+export interface Place {
+  id: number;
+  name: string;
+  lat: number;
+  lng: number;
+  address: string;
+  isFav: boolean;
+  createdAt: string;
+  updatedAt: string | null;
 }
 
 export interface Verification {
