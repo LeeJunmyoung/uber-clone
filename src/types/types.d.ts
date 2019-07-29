6f788686e143f848a96ad373514c32810bbcfb9c
leejm1 <wnsaud6016@naver.com>
2019-07-29 22:49:24 +0900

    Ride entity

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,6 +1,3 @@
+export type verificationTarget = "PHONE" | "EMAIL";
 
-
-const PHONE : string = "PHONE";
-const EMAIL : string = "EMAIL";
-
-export type verificationTarget = PHONE | EMAIL;
+export type rideStatus = "ACCEPTED" | "FINISHED" | "CANCELED" | "REQUESTING" | "ONROUTE";
